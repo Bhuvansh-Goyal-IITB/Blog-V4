44e370f397430e84c4e59d32d3a04d9d7f4f25c0
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2023-03-26 15:24:39 +0530

    Debugging create all

--- main.py
+++ main.py
@@ -22,9 +22,13 @@ Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @login_manager.user_loader
